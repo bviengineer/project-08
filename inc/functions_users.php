8855cef0b9e5a1c5af8c7593d6ff70ddc1786ccf
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-18 13:38:07 -0500

    Update functions_users.php
    add function to create a user and add user to the users table

--- inc/functions_users.php
+++ inc/functions_users.php
@@ -14,5 +14,20 @@ function findUserByEmail($email) {
     } catch (\Exception $e) {
         throw $e;
     }
+}
 
+function createUser($email, $password) {
+    global $db;
+
+    try {
+        $query = $db->prepare('INSERT INTO users (email, password, role_id) VALUES (:email, :password, 2)' );
+        $result->prepare($query);
+        $result->bindParam(':email', $email);
+        $result->bindParam(':password', $password);
+        $result->execute();
+        return findUserByEmail($emamil);
+
+    } catch (\Exception $e) {
+        throw $e;
+    }
 }
