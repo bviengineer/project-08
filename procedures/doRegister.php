012c870fb44ce63a3217a37dbaa76df86f8931aa
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-18 13:41:33 -0500

    Update doRegister.php
    -correct spelling of word request
    -add var to hold new user
    -Add applicable comments

--- procedures/doRegister.php
+++ procedures/doRegister.php
@@ -2,9 +2,9 @@
 
 require __DIR__ . '/../inc/bootstrap.php';
 
-$password->requet()->get('password');
-$confirmPassword->requet()->get('confirm_password');
-$email->requet()->get('email');
+$password->request()->get('password');
+$confirmPassword->request()->get('confirm_password');
+$email->request()->get('email');
 
 if ($password != $confirmPassword) {
     redirect('/register.php'); 
@@ -15,4 +15,8 @@ if (!empty($user)) {
     redirect('/register.php');
 }
 
+// Hasing of user password
 $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+// Creating a new user
+$user = createUser($email, $hashedPwd);
