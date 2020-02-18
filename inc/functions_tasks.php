7768d91d255f0bec7af93d2a55d07f85390f5856
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-18 10:36:56 -0500

    Update functions_tasks.php
    add code to verify whether user email address matches

--- inc/functions_tasks.php
+++ inc/functions_tasks.php
@@ -106,5 +106,16 @@ function deleteTask($task_id)
 
 
 function findUserByEmail($email) {
+    global $db;
+
+    try {
+        $query = $db->prepare('SELECT * from users where email = :email');
+        $query->bindParam(':email', $email);
+        $quey->excute();
+        return $query->fetch(PDO::FETCH_ASSOC);
+
+    } catch (\Exception $e) {
+        throw $e;
+    }
 
 }
