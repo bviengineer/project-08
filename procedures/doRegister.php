a1323c76caaae0938fac25bb9b664d163e915d22
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-18 09:39:57 -0500

    add conditional to doRegister.php
    add code to verify whether the current pwd is the same as the confirmed pwd

--- procedures/doRegister.php
+++ procedures/doRegister.php
@@ -5,3 +5,7 @@ require __DIR__ . '/../inc/bootstrap.php';
 $password->requet()->get('password');
 $confirmPassword->requet()->get('confirm_password');
 $email->requet()->get('email');
+
+if ($password != $confirmPassword) {
+    redirect('/register.php'); 
+}
