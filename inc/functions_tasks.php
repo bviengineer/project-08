d0de125d535e8925022b82b01a23472bf2fb3afe
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-18 11:38:09 -0500

    Update functions_tasks.php
    remove the findUerByEmail function from file

--- inc/functions_tasks.php
+++ inc/functions_tasks.php
@@ -103,19 +103,3 @@ function deleteTask($task_id)
     }
     return true;
 }
-
-
-function findUserByEmail($email) {
-    global $db;
-
-    try {
-        $query = $db->prepare('SELECT * from users where email = :email');
-        $query->bindParam(':email', $email);
-        $quey->excute();
-        return $query->fetch(PDO::FETCH_ASSOC);
-
-    } catch (\Exception $e) {
-        throw $e;
-    }
-
-}
