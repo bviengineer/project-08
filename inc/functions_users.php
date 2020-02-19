84b7d79154723644e1f2c6b807b067200804457e
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-19 08:47:05 -0500

    Update functions_users.php
    -corrected syntax errors

--- inc/functions_users.php
+++ inc/functions_users.php
@@ -4,30 +4,24 @@
 
 function findUserByUsername($username) {
     global $db;
-
     try {
         $query = $db->prepare('SELECT * from users where username = :username');
         $query->bindParam(':username', $username);
         $query->execute();
         $result = $query->fetch(PDO::FETCH_ASSOC);
         return $result;
-
     } catch (\Exception $e) {
         throw $e;
     }
 }
-
 function createUser($username, $password) {
     global $db;
-
     try {
         $query = $db->prepare('INSERT INTO users (username, password, role_id, task_id) VALUES (:username, :password, 2, 1)' );
-        $result = $db->prepare($query);
-        $result->bindParam(':username', $username);
-        $result->bindParam(':password', $password);
-        $result->execute();
-        //return findUserByUsername($username);
-
+        $query->bindParam(':username', $username);
+        $query->bindParam(':password', $password);
+        $query->execute();
+        return findUserByUsername($username);
     } catch (\Exception $e) {
         throw $e;
     }
