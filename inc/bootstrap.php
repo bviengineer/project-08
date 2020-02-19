f4289b8234689b7931c1f1d197eef17656420e53
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-19 11:01:57 -0500

    Update bootstrap.php
    Add missing $ from variable name $extra

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -35,7 +35,7 @@ function request() {
 function redirect($path, $extra = []) {
     $response = \Symfony\Component\HttpFoundation\Response::create(null, \Symfony\Component\HttpFoundation\Response::HTTP_FOUND, ['Location' => $path]);
     if (key_exists('cookies', $extra)){
-        foreach (extra['cookies'] as $cookie) {
+        foreach ($extra['cookies'] as $cookie) {
             $response->headers->setCookie($cookie);
         }
     }
