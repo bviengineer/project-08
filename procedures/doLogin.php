bb0a8b5c3cf2fb8a6d98ff58aa0ea37d5f832fba
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-19 09:50:24 -0500

    Update doLogin.php > verify password
    Add code to verify whether provided password matches hased version of stored password

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -1,7 +1,13 @@
 <?php 
 require __DIR__.'/../inc/bootstrap.php';
 
+// Verifies whether the username provided alreaady exist in the database
 $user = findUserByUsername(request()->get('username'));
 if (!empty($user)) {
     redirect('/register.php');
+}
+
+// Verfies whether the provided password matches the stored hashed password
+if (!password_verify(request()->get('password'), $user['password'])) {
+    redirect('/login.php');
 }
