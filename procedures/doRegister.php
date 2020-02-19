a27c5948e6c623a591bac402f1a7660ef83c1599
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-19 08:51:43 -0500

    Update doRegister.php
    -Update comments
    -Correct syntax errors

--- procedures/doRegister.php
+++ procedures/doRegister.php
@@ -1,27 +1,26 @@
-<?php
+<?php 
+require __DIR__.'/../inc/bootstrap.php';
 
-require __DIR__ . '/../inc/bootstrap.php';
+$password = request()->get('password');
+$confirmPassword = request()->get('confirm_password');
+$username = request()->get('username');
 
-$password->request()->get('password');
-$confirmPassword->request()->get('confirm_password');
-$username->request()->get('username');
-
-// Verify if password matches 
+// Verify whether passwords match, redirects if different 
 if ($password != $confirmPassword) {
     redirect('/register.php'); 
 }
 
-// Checks if a user already exists with that username 
+// Hasing of user password if existing an existing user is not found 
+$hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+// Verifies whether a user already exists with that username, redirects if there is 
 $user = findUserByUsername($username);
 if (!empty($user)) {
     redirect('/register.php');
 }
 
-// Hasing of user password
-$hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-
-// Calling create user function
+// Adds new user to database if password & username verification steps are successful
 $user = createUser($username, $hashedPwd);
 
-// Redirect user to the home page
+// Redirect user to the home page after creating user 
 redirect('/');
