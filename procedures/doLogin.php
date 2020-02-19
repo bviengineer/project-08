688febb14524a47252bcc304b82d3c217924e640
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-19 09:52:52 -0500

    Update doLogin.php > change file name
    change php file name in redirect to login.php

--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -4,7 +4,7 @@ require __DIR__.'/../inc/bootstrap.php';
 // Verifies whether the username provided alreaady exist in the database
 $user = findUserByUsername(request()->get('username'));
 if (!empty($user)) {
-    redirect('/register.php');
+    redirect('/login.php');
 }
 
 // Verfies whether the provided password matches the stored hashed password
