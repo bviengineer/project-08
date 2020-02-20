c1e62892ee4523d455f9ccfcd1a740347618456b
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-20 14:01:56 -0500

    Update functions_users.php
    add functionality to require authentication

--- inc/functions_users.php
+++ inc/functions_users.php
@@ -26,3 +26,28 @@ function createUser($username, $password) {
         throw $e;
     }
 }
+
+function isAuthenticated() {
+    if (!request()->cookies->has('access_token')) {
+        return false;
+    }
+
+    try {
+        \Firebase\JWT\JWT::$leeway = 1;
+        \Firebase\JWT\JWT::decode(
+            request()->cookies->get('access_token'),
+            getenv('SECRET_KEY'),
+            ['HS256']
+        );
+        return true;
+    } catch (\Exception $e) {
+        return false;
+    }
+}
+
+function requireAuth() {
+    if (!isAuthenticated()) {
+        $accessToken = new \Symfony\Component\HttpFoundation\Cookie('access_token', 'Expired', time()-3600, '/', getenv('COOKIE_DOMAIN'));
+        redirect("/login.php", ['cookies'=> [$accessToken]]);
+    }
+}
