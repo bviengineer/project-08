f9bd97f8472573ff0563d2873bd87df711290ec7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-20 14:11:30 -0500

    Update doLogout.php
    add access token  code to doLogin.php

--- procedures/doLogout.php
+++ procedures/doLogout.php
@@ -1,2 +1,5 @@
 <?php
 require __DIR__ . '/../inc/boostrap.php';
+
+$accessToken = new \Symfony\Component\HttpFoundation\Cookie('access_token', 'Expired', time()-3600, '/', getenv('COOKIE_DOMAIN'));
+        redirect("/login.php", ['cookies'=> [$accessToken]]);
