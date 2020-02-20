045bb96e856323bb673e5c603a25306af893f1b7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-20 14:28:42 -0500

    Update header.php
    add conditional to determine if user is logged in our logged out in order to determine between which state should be displayed: login or logout.

--- inc/header.php
+++ inc/header.php
@@ -58,9 +58,13 @@
       </ul>
         <ul class="nav">
             <li class="nav-item task<?php if ($page == "account") { echo " on"; } ?>"><a class="nav-link" href="/account.php">My Account</a></li>
+            <?php if (isAuthenticated()) : ?>
             <li class="nav-item tasks"><a class="nav-link" href="/inc/doLogout.php">Logout</a></li>
-            <li class="nav-item tasks<?php if ($page == "login") { echo " on"; } ?>"><a class="nav-link" href="/login.php">Login</a></li>
+            <?php else: ?>
+            <li class="nav-item tasks<?php if ($page == "login") { echo " on"; } ?>"><a 
+            class="nav-link" href="/login.php">Login</a></li>
             <li class="nav-item tasks<?php if ($page == "register") { echo " on"; } ?>"><a class="nav-link" href="/register.php">Register</a></li>
+            <?php endif; ?>
         </ul>
     </div>
   </header>
