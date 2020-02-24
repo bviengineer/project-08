40f959d3840566f99f3ff6dbdf361435ac0c429c
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-24 05:02:38 -0500

    Update doLogout.php
    correct spelling of bootstrap.php

--- procedures/doLogout.php
+++ procedures/doLogout.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../inc/boostrap.php';
+require __DIR__ . '/../inc/bootstrap.php';
 
 $accessToken = new \Symfony\Component\HttpFoundation\Cookie('access_token', 'Expired', time()-3600, '/', getenv('COOKIE_DOMAIN'));
         redirect("/login.php", ['cookies'=> [$accessToken]]);
