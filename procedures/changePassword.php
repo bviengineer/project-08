1b578a76d55a879d9f8767e194c0436b96795153
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-24 11:10:46 -0500

    Update changePassword.php
    verify whether new and current passwords match

--- procedures/changePassword.php
+++ procedures/changePassword.php
@@ -5,3 +5,8 @@ requireAuth();
 $currentPassword = request()->get('current_password');
 $newPassword = request()->get('password');
 $confirmedPassword = request()->get('confirm_password');
+
+if ($newPassword != $confirmedPassword) {
+    $session->getFlashBag()->add('error', 'New passwords do not match, please try again');
+    redirect('/account.php');
+}
