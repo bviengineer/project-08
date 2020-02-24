9a5f1cd5c64e40b88a97b7e0d95af874fe48d73e
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-24 15:21:38 -0500

    Update changePassword.php
    -Add comments
    -Add conditional to handler an empty user value
    -Add code to hash newly supplied password

--- procedures/changePassword.php
+++ procedures/changePassword.php
@@ -6,27 +6,37 @@ $currentPassword = request()->get('current_password');
 $newPassword = request()->get('password');
 $confirmedPassword = request()->get('confirm_password');
 
+
+// Verifies whether new password matches the confirmed version of the new password
 if ($newPassword != $confirmedPassword) {
     $session->getFlashBag()->add('error', 'New passwords do not match, please try again');
     redirect('/account.php');
 }
 
+// Retrieves user using the 'sub' of the jwt
 $user = findUserByAccessToken();
+
+// If a username could not be found 
 if (empty($user)) {
-    $session->getFlashBag()->add('error', 'Some error Happened. Try again.  If it continues, please log out and log back in.');
+    $session->getFlashBag()->add('error', 'Some error Happened. Try again.  If it continues, please 
+    log out and log back in.');
     redirect('/account.php');
 }
 
+// Verifies whether the password in the database matches the user supplied password
 if (!password_verify($currentPassword, $user['password'])) {
     $session->getFlashBag()->add('error', 'Current password is incorrect. Please try again');
     redirect('/account.php');
 }
 
-$updated = updatePassword(password_hash($newPassword, PASSWORD_DEFAULT), $user['id']);
+// Hashes the newly updated password
+$updated = updatePassword(password_hash($newPassword, PASSWORD_DEFAULT), $user['username']);
 
+// If password could not be updated
 if (!$updated) {
     $session->getFlashBag()->add('error', 'Could not update password. Please try again.');
     redirect('/account.php');
 }
+// Displays success message if the password was updted successfully 
 $session->getFlashBag()->add('success', 'Password updated');
 redirect('/account.php');
