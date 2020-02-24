e87ffcd86ecdc65044bd2ffa636ba9580805787f
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-24 11:55:13 -0500

    Update changePassword.php
    Add check to find user by access token

--- procedures/changePassword.php
+++ procedures/changePassword.php
@@ -10,3 +10,9 @@ if ($newPassword != $confirmedPassword) {
     $session->getFlashBag()->add('error', 'New passwords do not match, please try again');
     redirect('/account.php');
 }
+
+$user = findUserByAccessToken();
+if (empty($user)) {
+    $session->getFlashBag()->add('error', 'Some error Happened. Try again.  If it continues, please log out and log back in.');
+    redirect('/acoount.php');
+}
