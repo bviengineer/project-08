c89d458564a904c28f0a573039bf4d02b1b883c4
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-24 10:51:43 -0500

    Update changePassword.php
    -Add requireAuth()
    -Get current, new and confirmed passwords from form

--- procedures/changePassword.php
+++ procedures/changePassword.php
@@ -1,2 +1,7 @@
 <?php
 require_once __DIR__ . '/../inc/bootstrap.php';
+requireAuth();
+
+$currentPassword = request()->get('current_password');
+$newPassword = request()->get('password');
+$confirmedPassword = request()->get('confirm_password');
