bb93dd289c1bd40c5ecebc1631239df7a4cf0f67
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-24 13:29:51 -0500

    Update changePassword.php
    -Add var to hold result of updatePassword() function
    -Add conditional to display error message if password update failed
    -Add flast bag msg for a successfully updated pwd

--- procedures/changePassword.php
+++ procedures/changePassword.php
@@ -21,3 +21,12 @@ if (!password_verify($currentPassword, $user['password'])) {
     $session->getFlashBag()->add('error', 'Current password is incorrect. Please try again');
     redirect('/account.php');
 }
+
+$updated = updatePassword(password_hash($newPassword, PASSWORD_DEFAULT), $user['id']);
+
+if (!$updated) {
+    $session->getFlashBag()->add('error', 'Could not update password. Please try again.');
+    redirect('/account.php');
+}
+$session->getFlashBag()->add('success', 'Password updated');
+redirect('/account.php');
