87d1e33eb700d41eab9fab914ee7055bd5c888a7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2020-02-27 13:54:41 -0500

    Update functions_users.php
    Remove from INSERT INTO sql statent, insertion of data for task_id column

--- inc/functions_users.php
+++ inc/functions_users.php
@@ -45,7 +45,7 @@ function findUserByAccessToken() {
 function createUser($username, $password) {
     global $db;
     try {
-        $query = $db->prepare('INSERT INTO users (username, password, role_id, task_id) VALUES (:username, :password, 2, 1)' );
+        $query = $db->prepare('INSERT INTO users (username, password, role_id) VALUES (:username, :password, 2)' );
         $query->bindParam(':username', $username);
         $query->bindParam(':password', $password);
         $query->execute();
